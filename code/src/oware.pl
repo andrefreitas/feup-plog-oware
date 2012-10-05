3d659dbdca28daf06185460a6c7468545b35c5d9
Andre Freitas <p.andrefreitas@gmail.com>
2012-10-05 18:34:07 +0100

    Adicionado um logo para o jogo

--- code/src/oware.pl
+++ code/src/oware.pl
@@ -2,7 +2,7 @@
 initBoard([[4,4,4,4,4,4],[4,4,4,4,4,4]]).
 
 % Print the board
-% Test with: printBoard([[4,4,4,4,4,4],[4,4,4,4,4,4]]).
+% Test with: initBoard(B),printBoard(B).
 printBoardLine([]).
 printBoardLine([H|T]):-
 	write(' ( '),
@@ -10,13 +10,23 @@ printBoardLine([H|T]):-
 	write(' ) '),
 	printBoardLine(T).
 
+printLogo:-
+	write('\n\n'),
+	write('           _____      ____ _ _ __ ___\n'), 
+	write('          / _ \\ \\ /\\ / / _` |  __/ _ \\ \n'),
+	write('         | (_) \\ V  V / (_| | | |  __/ \n'),
+	write('          \\___/ \\_/\\_/ \\__,_|_|  \\___| \n\n\n').
+	
 printBoard([H|[Th|Tt]]):-
-	write('  /------------------------------------------\\ \n'),
-	write(' / '),
-	printBoardLine(H),
-	write(' \\ \n'),
+	printLogo,
+	write('\n'),
+	write('                    Player 1 \n\n'),
+	write('   /----------------------------------------\\ \n'),
+	write(' / '), printBoardLine(H),write(' \\ \n'),
 	write('|----------------------------------------------|'),
-	write('\n \\ '),
-	printBoardLine(Th),
-	write(' / \n'),
-	write('  \\------------------------------------------/'). 
+	write('\n \\ '), printBoardLine(Th), write(' / \n'),
+	write('   \\----------------------------------------/\n\n'),
+	write('                    Player 2 \n\n\n\n').
+
+
+	
