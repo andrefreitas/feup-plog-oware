7ff6058573b276c53550f0424eb23fcf10ba9a7d
Andre Freitas <p.andrefreitas@gmail.com>
2012-10-06 10:15:40 +0100

    Adicionadas funções para adicionar e remover sementes numa dada posição no tabuleiro.

--- code/src/oware.pl
+++ code/src/oware.pl
@@ -28,7 +28,7 @@ printBoard([H|[Th|_]]):-
 	write('   \\----------------------------------------/\n\n'),
 	write('                    Player 2 \n\n\n\n').
 
-% Core List Functions
+% List Core Functions
 replace([_|T],0,X,[X|T]).
 replace([H|T],N,X,[H|T2]):- 
 	N>0, 
@@ -50,4 +50,25 @@ elementMinus(List,I,Value,NewList):-
 	element(List,I,Oldval),
 	Newval is Oldval-Value,
 	replace(List,I,Newval,NewList).
+
+% In game functions
+initScore(0).
+addScore(Score,Val,NewScore):-NewScore is Score+Val.
+
+addSeeds(PlayerNum,[H|[Th|Tt]],I,Seeds,[Hnew|Tnew]):-
+	PlayerNum=1,
+	elementPlus(H,I,Seeds,Hnew),
+	Tnew = [Th|Tt];
+	PlayerNum=2,
+	elementPlus(Th,I,Seeds,Tnew),
+	Hnew = H.
 	
+removeSeeds(PlayerNum,[H|[Th|Tt]],I,Seeds,[Hnew|Tnew]):-
+	PlayerNum=1,
+	elementMinus(H,I,Seeds,Hnew),
+	Tnew = [Th|Tt];
+	PlayerNum=2,
+	elementMinus(Th,I,Seeds,Tnew),
+	Hnew = H.
+		
+% distributeSeeds(PlayerNum,Board,I,NewBoard):-
