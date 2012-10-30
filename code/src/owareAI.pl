611473233fb95e62a8c7344dcb77b018b5aa0dbd
Andre Freitas <p.andrefreitas@gmail.com>
2012-10-30 17:32:28 +0000

    Criada a inteligencia artificial.

--- code/src/owareAI.pl
+++ code/src/owareAI.pl
@@ -6,23 +6,46 @@
 % | |__| |\ V  V / (_| | | |  __/
 %  \____/  \_/\_/ \__,_|_|  \___| Artificial Intelligence
 %
-%
-%              _/       \_
-%             / |       | \
-%            /  |__   __|  \
-%           |__/((o| |o))\__|
-%           |      | |      |
-%           |\     |_|     /| 
-%           | \           / |  Look at me, i can compute...
-%            \| /  ___  \ |/
-%             \ | / _ \ | /
-%              \_________/
-%               _|_____|_
-%          ____|_________|____
-%          /                   \ 
-%
 % Authors:
 % 		André Freitas - ei10036@fe.up.pt
 %		Rui Goncalves - ei10100@fe.up.pt
 %
 %  ***************************************************************
+
+% Include modules
+:- consult(owareBoard).
+:- consult(oware).
+% aiPlay/4
+aiPlay(PlayerNum,Board,Pos):-
+	aiTryAll(Board, PlayerNum, ScoreList,0),
+	scoreListGetPos(ScoreList,1,Pos,_).
+	
+aiTryAll(_,_,ScoreList,6):-
+	ScoreList=[].
+
+% N[0-5]
+aiTryAll(Board, PlayerNum, ScoreList,N):-
+	playSeeds(Board,PlayerNum,N,_,Score),
+	%write('N: '), write(N),write(' - Score: '), write(Score),nl,
+	NewN is N+1,
+	aiTryAll(Board,PlayerNum,NewScoreList,NewN),
+	append([Score],NewScoreList,ScoreList).
+
+% MaxScorePos[1-6]
+scoreListGetPos(ScoreList,6,MaxScorePos,MaxScore):-
+	ScoreList=[Score|_],
+	MaxScore=Score,
+	MaxScorePos=6.
+
+scoreListGetPos(ScoreList,N,MaxScorePos,MaxScore):-
+	ScoreList=[Score|T],
+	NewN is N+1,
+	scoreListGetPos(T,NewN,MaxScorePosNext,MaxScoreNext),
+	(
+		Score > MaxScoreNext,
+			MaxScorePos=N,
+			MaxScore=Score;
+		% else
+			MaxScorePos=MaxScorePosNext,
+			MaxScore=MaxScoreNext
+	).
